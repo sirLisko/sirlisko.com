3e7a0fba460cad1f5f1054be8d938dcbb9103495
sirLisko <sirlisko@gmail.com>
2013-07-05 16:28:03 +0100

    fix twitter, damned them and their oauth

--- src/javascripts/twitter.js
+++ src/javascripts/twitter.js
@@ -1,16 +1,10 @@
-/*global $*/
-/*jslint unused: false*/
+/*global jQuery, Siutsur*/
 
-function twitterCallback (tweet) {
-	'use strict';
-	$('.tweet .baloon').html(tweet[0].text
-		.replace(/(\b(https?|ftp|file):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/ig, '<a href="$1">$1</a>')
-		.replace(/[@]+[A-Za-z0-9.-_]+/g, function (u) {
-			var username = u.replace('@', '');
-			return u.link('http://twitter.com/' + username);
-		}).replace(/[#]+[A-Za-z0-9.-_]+/g, function (t) {
-			var tag = t.replace('#', '%23');
-			return t.link('http://search.twitter.com/search?q=' + tag);
-		})
-	);
-}
+(function($){
+	"use strict";
+
+	Siutsur.load('353168929520230401', function(obj) {
+		$('.tweet .baloon').html(obj[0].title.regular);
+	}, function() {});
+
+})(jQuery);
