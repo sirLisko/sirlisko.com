a850685f0d35be28e900cec24eda306611ed8223
sirLisko <sirlisko@gmail.com>
2013-07-07 21:15:58 +0100

    fixed twitter api

--- src/javascripts/twitter.js
+++ src/javascripts/twitter.js
@@ -1,10 +1,23 @@
-/*global jQuery, Siutsur*/
+/*global $*/
+/*jslint unused: false*/
 
-(function($){
-	"use strict";
+function twitterCallback (tweet) {
+	'use strict';
 
-	Siutsur.load('353168929520230401', function(obj) {
-		$('.tweet .baloon').html(obj[0].title.regular);
-	}, function() {});
+	tweet = tweet[0].text
+		.replace(/(\b(https?|ftp|file):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/ig, '<a href="$1">$1</a>')
+		.replace(/[@]+[A-Za-z0-9.-_]+/g, function (u) {
+			var username = u.replace('@', '');
+			return u.link('http://twitter.com/' + username);
+		}).replace(/[#]+[A-Za-z0-9.-_]+/g, function (t) {
+			var tag = t.replace('#', '%23');
+			return t.link('http://search.twitter.com/search?q=' + tag);
+		});
 
-})(jQuery);
+	$('.tweet .baloon').html(tweet);
+}
+
+$.ajax('/twitter', {
+	dataType: 'json',
+	success: twitterCallback
+});
