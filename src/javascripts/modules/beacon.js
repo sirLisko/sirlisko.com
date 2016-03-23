27ab89c46046dcd082e0c43231a8a5f7ad1dfcfe
Luca Lischetti <sirlisko@gmail.com>
2016-03-23 21:46:42 +0000

    improved outbound beacon

--- src/javascripts/modules/beacon.js
+++ src/javascripts/modules/beacon.js
@@ -1,14 +1,20 @@
 /*global ga*/
 'use strict';
 
-function beacon(category, action) {
-	ga('send', 'event', category, action);
+function beacon(category, action, label, value, fieldsObject) {
+	ga('send', 'event', category, action, label, value, fieldsObject);
 }
 
-document.addEventListener('click', function(e, bcn){
-	if ((bcn = e.target.getAttribute('data-beacon'))) {
-		beacon('outgoing', bcn);
+function beaconOutbound(e) {
+	if ((e.target.getAttribute('data-beacon') != undefined)) {
+		var url = e.target.getAttribute('href');
+		beacon('outbound', 'click', url, {
+			transport: 'beacon',
+			hitCallback: function(){ document.location = url; }
+		});
 	}
-});
+}
+
+document.addEventListener('click', beaconOutbound);
 
 module.exports = beacon;
--- test/beaconTest.js
+++ test/beaconTest.js
@@ -4,16 +4,17 @@ var test = require('tape');
 var fakeEvent = require('simulant');
 
 test('testig outgoing beacon on anchor click', function (t) {
-	t.plan(4);
+	t.plan(5);
 
-	window.ga = function(send, event, category, action){
+	window.ga = function(send, event, category, action, label){
 		t.equal(send, 'send', 'google action');
 		t.equal(event, 'event', 'google type');
-		t.equal(category, 'outgoing', 'the category is outgoing');
-		t.equal(action, 'foo', 'the beacon is correct');
+		t.equal(category, 'outbound', 'the category is outgoing');
+		t.equal(action, 'click', 'the action is correct');
+		t.equal(label, 'foo', 'the label contains the url');
 	};
 
-	document.body.innerHTML = '<div data-beacon="foo"></div>';
+	document.body.innerHTML = '<a href="foo" data-beacon></a>';
 
 	require('../src/javascripts/modules/beacon');
 
