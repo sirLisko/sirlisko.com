984665bd0fe51db69624d502a1be275a9aa6c80d
Luca Lischetti <luca@sirlisko.com>
2016-06-12 15:13:16 +0100

    fixed test with fakeTimers

--- test/switchTitleTest.js
+++ test/switchTitleTest.js
@@ -1,9 +1,11 @@
 'use strict'
 
 var test = require('tape')
+var sinon = require('sinon')
 
 test('the switch is done after 5s', function (t) {
   t.plan(4)
+  var fakeTimer = sinon.useFakeTimers()
 
   document.body.innerHTML = '<div class="me__active me__intro--active"></div><div class="me__intro"><span>foo</span></div>'
 
@@ -12,8 +14,10 @@ test('the switch is done after 5s', function (t) {
 
   require('../src/javascripts/modules/switchTitle')
 
-  setTimeout(function () {
-    t.notOk(document.querySelector('.me__active').classList.contains('me__intro--active'), 'me__active is not active anymore')
-    t.ok(document.querySelector('.me__intro span').classList.contains('me__intro--active'), 'the real me__intro is now active')
-  }, 5000)
+  fakeTimer.tick(5001)
+
+  t.notOk(document.querySelector('.me__active').classList.contains('me__intro--active'), 'me__active is not active anymore')
+  t.ok(document.querySelector('.me__intro span').classList.contains('me__intro--active'), 'the real me__intro is now active')
+
+  fakeTimer.restore()
 })
