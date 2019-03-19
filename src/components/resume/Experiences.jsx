a79f40fbe0c9592b189e9c772e8a0e07252f5822
Luca Lischetti <luca@sirlisko.com>
2019-03-19 17:31:01 +0000

    reshaped the resume page

--- src/components/resume/Experiences.jsx
+++ src/components/resume/Experiences.jsx
@@ -6,8 +6,11 @@ const Experiences = ({ experiences }) => (
     <h1>Experiences</h1>
     {experiences.map(exp => (
       <div key={exp.where || exp.role}>
-        {exp.where && <h2>{exp.where}</h2>}
-        {exp.when && <h4>{exp.when}</h4>}
+        {exp.where && (
+          <h2>
+            {exp.where} {exp.when && <span>{exp.when}</span>}
+          </h2>
+        )}
         {exp.blurp.map((blr, i) => (
           <p key={i}>{blr}</p>
         ))}
