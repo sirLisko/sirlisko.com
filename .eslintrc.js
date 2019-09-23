600701b456333d18789fa063cda735041d55a18a
Luca Lischetti <luca@sirlisko.com>
2019-09-23 19:18:25 +0200

    added babel parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {}
+};
+
+module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:import/errors",
@@ -25,5 +47,6 @@ module.exports = {
     browser: true,
     es6: true,
     node: true
-  }
+  },
+  parser: "babel-eslint"
 };
