69e9446c1abe4f123fc612174180376133dc04eb
Luca Lischetti <a-luca.lischetti@rvu.co.uk>
2020-02-03 17:59:27 +0000

    updated experiences

--- src/components/resume/Contacts.jsx
+++ src/components/resume/Contacts.jsx
@@ -5,14 +5,22 @@ const Contacts = ({ isAltVersion }) => (
   <article className="section contacts">
     <h1>Contacts</h1>
     <p>
-      My website <a href="https://sirlisko.com">https://sirlisko.com</a>
+      My personal website{" "}
+      <a href="https://sirlisko.com">https://sirlisko.com</a>
     </p>
     <p>
       My Github{" "}
       <a href="https://github.com/sirlisko">https://github.com/sirlisko</a>
     </p>
     <p>
-      My email address <a href="mailto:luca@sirlisko.com">luca@sirlisko.com</a>.
+      My LinkedIn{" "}
+      <a href="https://www.linkedin.com/in/lucalischetti/">
+        https://www.linkedin.com/in/lucalischetti/
+      </a>
+    </p>
+    <p>
+      Drop me an email at{" "}
+      <a href="mailto:luca@sirlisko.com">luca@sirlisko.com</a>.
     </p>
     {!isAltVersion ? (
       <p>
