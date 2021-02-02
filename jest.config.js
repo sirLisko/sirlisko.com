b11b617e4ccfe8e7140ef84bdbe9171cc58ad619
Luca Lischetti <luca@sirlisko.com>
2021-02-02 23:23:16 +0000

    simplified jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  transform: {
-    "^.+\\.tsx?$": `<rootDir>/jest-preprocess.js`,
-  },
+  preset: "ts-jest",
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
   testPathIgnorePatterns: [`node_modules`, `.cache`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
-  globals: {
-    __PATH_PREFIX__: ``,
-  },
   testURL: `http://localhost`,
-  setupFiles: [`<rootDir>/loadershim.js`],
 };
